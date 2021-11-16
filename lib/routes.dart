7420cb5ff917534fe0f27b2e902c4a606e1c324c
ziperjohn <honzavaverka1996@gmail.com>
2021-11-16 23:13:20 +0100

    Created screens

---
+++ lib/routes.dart
@@ -0,0 +1,15 @@
+import 'dart:js';
+
+import 'package:quizapp/about/about.dart';
+import 'package:quizapp/home/home.dart';
+import 'package:quizapp/login/login.dart';
+import 'package:quizapp/profile/profile.dart';
+import 'package:quizapp/topics/topics.dart';
+
+var appRoutes = {
+  "/": (context) => const HomeScreen(),
+  "/login": (context) => const LoginScreen(),
+  "/topics": (context) => const TopicsScreen(),
+  "/profile": (context) => const ProfileScreen(),
+  "/about": (context) => const AboutScreen(),
+};
