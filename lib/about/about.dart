64fb095e71544a02a2f3e8015b4e4e8917d2ff22
ziperjohn <honzavaverka1996@gmail.com>
2021-11-17 14:10:38 +0100

    Added scaffold to about screen

--- lib/about/about.dart
+++ lib/about/about.dart
@@ -5,6 +5,11 @@ class AboutScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("About"),
+        centerTitle: true,
+      ),
+    );
   }
 }
