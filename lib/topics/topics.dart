62c99b462341feefbe478e812405392e9229e6fd
ziperjohn <honzavaverka1996@gmail.com>
2021-11-17 14:08:57 +0100

    Created bottom nav bar

--- lib/topics/topics.dart
+++ lib/topics/topics.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:quizapp/shared/bottom_nav.dart';
 
 class TopicsScreen extends StatelessWidget {
   const TopicsScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Topics"),
+        centerTitle: true,
+      ),
+      bottomNavigationBar: const BottomNavBar(),
+    );
   }
 }
